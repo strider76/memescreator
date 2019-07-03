81b713b586d0683ca0bc94599740bed5d01d7a00
Manuel Millan <manuel@betangible.com>
2019-07-03 17:36:12 +0200

    actualizando  webpack

---
+++ src/js/general.js
@@ -0,0 +1,5 @@
+import '../css/styles.css';
+import 'bootstrap/less/bootstrap.less'; //incluye css y fuentes de bootstrap
+import 'bootstrap'; //para incluir js de Bootstrap
+
+console.log('General JS File');
---
+++ src/js/memes.js
@@ -0,0 +1,11 @@
+import './general';
+
+console.log('Memes JS file');
+
+class Memes {
+    constructor() {
+        console.log('Inside Memes class');
+    }
+}
+
+new Memes();
